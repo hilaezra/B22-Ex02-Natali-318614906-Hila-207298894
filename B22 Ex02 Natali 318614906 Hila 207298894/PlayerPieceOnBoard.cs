719126aeaff42582d694bf325234856af1a5b79c
Hila Ezra <hilaezra1998@gmail.com>
2022-04-09 18:48:27 +0300

    lklvcsss

---
+++ B22 Ex02 Natali 318614906 Hila 207298894/PlayerPieceOnBoard.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B22_Ex02_Natali_318614906_Hila_207298894
+{
+    class PlayerPieceOnBoard
+    {
+        private bool m_IsPieceIsKing;
+        private Point m_Point;//cordinate of piece
+
+        private static bool ValidMove(Point i_NextPosition)
+        {
+            bool validMove = true;
+
+            //TODO.....
+
+            return validMove;
+        }
+    }
+}
