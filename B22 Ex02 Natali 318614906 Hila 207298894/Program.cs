339c2bf25eac025ceb512b82dc91aaba33d9a6e2
Hila Ezra <hilaezra1998@gmail.com>
2022-04-09 15:03:27 +0300

    hth

---
+++ B22 Ex02 Natali 318614906 Hila 207298894/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B22_Ex02_Natali_318614906_Hila_207298894
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+        }
+    }
+}
