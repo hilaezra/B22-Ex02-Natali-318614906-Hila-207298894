9da146643c0d3764dd9b97441347441357b6576e
Natali Alizi <natalial@mta.ac.il>
2022-04-09 17:54:52 +0300

    HI

--- B22 Ex02 Natali 318614906 Hila 207298894/PieceOnBoard.cs
+++ B22 Ex02 Natali 318614906 Hila 207298894/PieceOnBoard.cs
@@ -8,5 +8,16 @@ namespace B22_Ex02_Natali_318614906_Hila_207298894
 {
     class PieceOnBoard
     {
+        private bool m_IsPieceIsKing;
+        private Point m_Point;//cordinate of piece
+
+        private static bool ValidMove(Point i_NextPosition)
+        {
+            bool validMove=true;
+
+            //TODO.....
+
+            return validMove;
+        }
     }
 }
