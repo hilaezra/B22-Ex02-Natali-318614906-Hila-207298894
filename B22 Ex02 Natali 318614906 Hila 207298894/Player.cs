36cc7370944ad4ff153006db259057f89af38745
Hila Ezra <hilaezra1998@gmail.com>
2022-04-09 17:33:04 +0300

    jhl

--- B22 Ex02 Natali 318614906 Hila 207298894/Player.cs
+++ B22 Ex02 Natali 318614906 Hila 207298894/Player.cs
@@ -8,27 +8,5 @@ namespace B22_Ex02_Natali_318614906_Hila_207298894
 {
     class Player
     {
-        private string m_NameOfPlayer;
-        private int m_PointsOfPlayer;
-        private int m_RemainPieces;
-        private char m_SignPlayer;
-
-        public Player(string i_Name, int i_SizeOfBorad, char i_Sign)
-        {
-            m_NameOfPlayer = i_Name;
-            m_PointsOfPlayer = 0;
-            m_RemainPieces = ((i_SizeOfBorad * i_SizeOfBorad) - 2 * i_SizeOfBorad) / 4;
-            m_SignPlayer = i_Sign;
-        }
-        public int PointsOfPlayer
-        {
-            get { return m_PointsOfPlayer; }
-            set { m_PointsOfPlayer = value; }
-        }
-        public int RemainPieces
-        {
-            get { return m_RemainPieces; }
-            set { m_RemainPieces = value; }
-        }
     }
 }
