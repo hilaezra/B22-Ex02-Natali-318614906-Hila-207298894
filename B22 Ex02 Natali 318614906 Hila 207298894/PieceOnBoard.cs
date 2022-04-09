a20b867b1c846d3448b9905fecc3465c642e378a
Hila Ezra <hilaezra1998@gmail.com>
2022-04-09 17:37:23 +0300

    fdz

---
+++ B22 Ex02 Natali 318614906 Hila 207298894/PieceOnBoard.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B22_Ex02_Natali_318614906_Hila_207298894
+{
+    class PieceOnBoard
+    {
+    }
+}
