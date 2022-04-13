71d3955f02c5f5fe863db3ed8556d81a4dc0b10f
Hila Ezra <hilaezra1998@gmail.com>
2022-04-13 22:50:29 +0300

    rehestr

--- B22 Ex02 Natali 318614906 Hila 207298894/CheckersGame.cs
+++ B22 Ex02 Natali 318614906 Hila 207298894/CheckersGame.cs
@@ -10,10 +10,28 @@ namespace B22_Ex02_Natali_318614906_Hila_207298894
     {
         private bool m_IsPlayingAgainstComputer;
 
+         public static void InitializationGame()
+        {
+            bool firstPlayer = true;
+            string nameOfPlayer = UserInputManagement.GetUserName(firstPlayer);
+            int sizeOfBoard = UserInputManagement.GetAndCheckValidBoardSize();
+            Player playerNumber1 = new Player(nameOfPlayer, sizeOfBoard, 'O', 1); ////PlayerNumber1IsReady
+            Player playerNumber2 = new Player("computer", sizeOfBoard, 'X', 2);
+            Board board = new Board(sizeOfBoard);
+            if (!UserInputManagement.PlayAgainstComputer())
+            {
+                firstPlayer = false;
+                nameOfPlayer = UserInputManagement.GetUserName(firstPlayer);
+                playerNumber2.NameOfPlayer = nameOfPlayer; ////SET
+            }
+
+            board.InitBoard();
+            RunGame(board, playerNumber1, playerNumber2);
+        }
         public static void RunGame(Board i_Board, Player i_PlayerNumber1, Player i_PlayerNumber2)
         {
             bool endGame = false, isEaten = false;
-            int i = 0, indexWhoEat = 1;
+            int i = 0;
             while (!endGame)
             {
                 isEaten = false;
@@ -21,28 +39,77 @@ namespace B22_Ex02_Natali_318614906_Hila_207298894
                 i_Board.PrintBoard(i_PlayerNumber1, i_PlayerNumber2);
                 i_PlayerNumber1.PrintPlayersDetails();
                 i_PlayerNumber2.PrintPlayersDetails();
-                string userMoveInString = UserInputManagement.PartOfTheBoardSquares(i_Board.BoardSize, i, i_PlayerNumber1, i_PlayerNumber2);
-                List<int> userMoverInInt = UserInputManagement.ChangedStringToListInt(userMoveInString);
-                if (i % 2 == 0)
+
+                if(i_PlayerNumber2.NameOfPlayer == "computer")
                 {
-                    indexWhoEat = 1;
-                    CheckPositionAndMove(i_PlayerNumber1, i_Board, userMoverInInt, ref isEaten, ref i, indexWhoEat);
+                    RunGameAgainstComputer(ref i, i_Board, i_PlayerNumber1, i_PlayerNumber2, ref isEaten, ref endGame);
                 }
                 else
                 {
-                    indexWhoEat = 2;
-                    CheckPositionAndMove(i_PlayerNumber2, i_Board, userMoverInInt, ref isEaten, ref i, indexWhoEat);
-                }
-
-                if (isEaten)
-                {
-                    CheckWhowEatAndUpdate(i_PlayerNumber1, i_PlayerNumber2, ref endGame, indexWhoEat);
+                    RunGameAgainstOtherPlayer(ref i, i_Board,  i_PlayerNumber1, i_PlayerNumber2, ref isEaten, ref endGame);
                 }
 
                 i++;
-
                 System.Threading.Thread.Sleep(500);
             }
+        }
+
+        public static void RunGameAgainstOtherPlayer(ref int i_WhichPlayer, Board i_Board, Player i_PlayerNumber1, Player i_PlayerNumber2, ref bool i_IsEaten, ref bool i_EndGame)
+        {
+            int indexWhoEat = 1;
+            string userMoveInString = UserInputManagement.PartOfTheBoardSquares(i_Board.BoardSize, i_WhichPlayer, i_PlayerNumber1, i_PlayerNumber2);
+            List<int> userMoverInInt = UserInputManagement.ChangedStringToListInt(userMoveInString);
+
+            if (i_WhichPlayer % 2 == 0) ///O
+            {
+                indexWhoEat = 1;
+                CheckPositionAndMove(i_PlayerNumber1, i_Board, userMoverInInt, ref i_IsEaten, ref i_WhichPlayer, indexWhoEat);
+            }
+          
+            else ///X
+            {
+                indexWhoEat = 2;
+                CheckPositionAndMove(i_PlayerNumber2, i_Board, userMoverInInt, ref i_IsEaten, ref i_WhichPlayer, indexWhoEat);
+            }
+
+            if (i_IsEaten)
+            {
+                CheckWhowEatAndUpdate(i_PlayerNumber1, i_PlayerNumber2, ref i_EndGame, indexWhoEat);
+            }
+        }
+
+        public static void RunGameAgainstComputer(ref int i_WhichPlayer, Board i_Board, Player i_PlayerNumber1, Player i_PlayerNumber2, ref bool i_IsEaten, ref bool i_EndGame)
+        {
+            if (i_WhichPlayer % 2 == 0) ///O
+            {
+                int indexWhoEat = 1;
+                string userMoveInString = UserInputManagement.PartOfTheBoardSquares(i_Board.BoardSize, i_WhichPlayer, i_PlayerNumber1, i_PlayerNumber2);
+                List<int> userMoverInInt = UserInputManagement.ChangedStringToListInt(userMoveInString);
+                indexWhoEat = 1;
+                CheckPositionAndMove(i_PlayerNumber1, i_Board, userMoverInInt, ref i_IsEaten, ref i_WhichPlayer, indexWhoEat);
+            }
+            else ///computer turn
+            {
+                ComputerMove(i_Board, i_PlayerNumber2, ref i_EndGame);
+            }
+        }
+
+        public static void ComputerMove(Board i_GameBoard, Player i_PlayerNumber1, Player i_PlayerNumber2, ref bool i_EndGame)
+        {
+            List<int> positionOfPlayerToEat = new List<int>(2);
+            bool someoneToEat = CheckIfThereIsSomeoneToEat(i_GameBoard, positionOfPlayerToEat);
+
+            if (someoneToEat == true)
+            {
+                Move(i_GameBoard, positionOfPlayerToEat);
+                UpdatePlayersAfterEaten(i_PlayerNumber2, i_PlayerNumber1, ref i_EndGame);
+            }
+            else
+            {
+                GetRandomPosition();
+                Move(i_GameBoard, positionOfPlayerToEat);
+            }
+
         }
 
         public static void CheckWhowEatAndUpdate(Player i_PlayerNumber1, Player i_PlayerNumber2, ref bool io_endGame, int i_NumberOfPlayer)
@@ -92,23 +159,6 @@ namespace B22_Ex02_Natali_318614906_Hila_207298894
             i_Player.PointsOfPlayer++;
         }
 
-        public static void InitializationGame()
-        {
-            bool firstPlayer = true;
-            string nameOfPlayer = UserInputManagement.GetUserName(firstPlayer);
-            int sizeOfBoard = UserInputManagement.GetAndCheckValidBoardSize();
-            Player playerNumber1 = new Player(nameOfPlayer, sizeOfBoard, 'O', 1); ////PlayerNumber1IsReady
-            Player playerNumber2 = new Player("computer", sizeOfBoard, 'X', 2);
-            Board board = new Board(sizeOfBoard);
-            if (!UserInputManagement.PlayAgainstComputer())
-            {
-                firstPlayer = false;
-                nameOfPlayer = UserInputManagement.GetUserName(firstPlayer);
-                playerNumber2.NameOfPlayer = nameOfPlayer; ////SET
-            }
-
-            board.InitBoard();
-            RunGame(board, playerNumber1, playerNumber2);
-        }
+       
     }
 }
